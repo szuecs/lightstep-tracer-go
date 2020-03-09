9ce98f769f817e7782993def57ccd5bd013e8260
Alex Boten <aboten@lightstep.com>
2020-03-09 15:11:49 -0700

    updated to use latest proto

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.12
 require (
 	github.com/gogo/protobuf v1.2.1
 	github.com/hashicorp/golang-lru v0.5.1 // indirect
-	github.com/lightstep/lightstep-tracer-common/golang/gogo v0.0.0-20190605223551-bc2310a04743
+	github.com/lightstep/lightstep-tracer-common/golang/gogo v0.0.0-20200305213919-a88bf8de3718
 	github.com/onsi/ginkgo v1.7.0
 	github.com/onsi/gomega v1.4.3
 	github.com/opentracing/opentracing-go v1.0.2
@@ -13,5 +13,3 @@ require (
 	go.opencensus.io v0.20.2
 	google.golang.org/grpc v1.21.0
 )
-
-replace github.com/lightstep/lightstep-tracer-common/golang/gogo => /Users/isobel/go/src/github.com/lightstep/lightstep-tracer-common/golang/gogo
--- internal/metrics/reporter.go
+++ internal/metrics/reporter.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/gogo/protobuf/proto"
 	"github.com/gogo/protobuf/types"
+	"github.com/lightstep/lightstep-tracer-common/golang/gogo/collectorpb"
 	"github.com/lightstep/lightstep-tracer-common/golang/gogo/metricspb"
 )
 
@@ -43,6 +44,7 @@ type Reporter struct {
 	address     string
 	timeout     time.Duration
 	accessToken string
+	stored      Metrics
 }
 
 func NewReporter(opts ...ReporterOption) *Reporter {
@@ -60,45 +62,48 @@ func NewReporter(opts ...ReporterOption) *Reporter {
 }
 
 func (r *Reporter) prepareRequest(m Metrics) (*metricspb.IngestRequest, error) {
-	start := time.Now()
 	idempotencyKey, err := generateIdempotencyKey()
 	if err != nil {
 		return nil, err
 	}
 	return &metricspb.IngestRequest{
 		IdempotencyKey: idempotencyKey,
-		ReporterId:     r.tracerID,
-		Labels:         r.attributes,
+		Reporter:       &collectorpb.Reporter{}, // TODO: fill in the reporter
+	}, nil
+}
+
+func addFloat(labels []*collectorpb.KeyValue, key string, value float64, start time.Time) *metricspb.MetricPoint {
+	return &metricspb.MetricPoint{
+		Kind:       metricspb.MetricKind_GAUGE,
+		MetricName: key,
+		Labels:     labels,
+		Value: &metricspb.MetricPoint_DoubleValue{
+			DoubleValue: value,
+		},
 		Start: &types.Timestamp{
 			Seconds: start.Unix(),
 			Nanos:   int32(start.Nanosecond()),
 		},
-		// TODO: how do we want to handle duration?
 		Duration: &types.Duration{
-			Seconds: 0,
-			Nanos:   0,
-		},
-	}, nil
-}
-
-func addFloat(labels map[string]string, key string, value float64) *metricspb.MetricPoint {
-	return &metricspb.MetricPoint{
-		Kind:          metricspb.MetricKind_GAUGE,
-		TimeSeriesKey: key,
-		Labels:        labels,
-		Value: &metricspb.MetricPoint_Float{
-			Float: value,
+			Seconds: 0, // TODO: set duration to number of retries * flush interval
 		},
 	}
 }
 
-func addUint(labels map[string]string, key string, value uint64) *metricspb.MetricPoint {
+func addUint(labels []*collectorpb.KeyValue, key string, value uint64, start time.Time) *metricspb.MetricPoint {
 	return &metricspb.MetricPoint{
-		Kind:          metricspb.MetricKind_GAUGE,
-		TimeSeriesKey: key,
-		Labels:        labels,
-		Value: &metricspb.MetricPoint_Uint{
-			Uint: value,
+		Kind:       metricspb.MetricKind_GAUGE,
+		MetricName: key,
+		Labels:     labels,
+		Value: &metricspb.MetricPoint_Uint64Value{
+			Uint64Value: value,
+		},
+		Start: &types.Timestamp{
+			Seconds: start.Unix(),
+			Nanos:   int32(start.Nanosecond()),
+		},
+		Duration: &types.Duration{
+			Seconds: 0, // TODO: set duration to number of retries * flush interval
 		},
 	}
 }
@@ -106,6 +111,7 @@ func addUint(labels map[string]string, key string, value uint64) *metricspb.Metr
 // Measure takes a snapshot of system metrics and sends them
 // to a LightStep endpoint.
 func (r *Reporter) Measure(ctx context.Context) error {
+	start := time.Now()
 	ctx, cancel := context.WithTimeout(ctx, r.timeout)
 	defer cancel()
 
@@ -119,35 +125,47 @@ func (r *Reporter) Measure(ctx context.Context) error {
 		return err
 	}
 
-	labels := map[string]string{
-		"name": "process.cpu",
+	labels := []*collectorpb.KeyValue{
+		&collectorpb.KeyValue{
+			Key:   "name",
+			Value: &collectorpb.KeyValue_StringValue{StringValue: "process.cpu"},
+		},
 	}
-	pb.Points = append(pb.Points, addFloat(labels, "cpu.user", m.ProcessCPU.User))
-	pb.Points = append(pb.Points, addFloat(labels, "cpu.system", m.ProcessCPU.System))
+	pb.Points = append(pb.Points, addFloat(labels, "runtime.go.cpu.user", m.ProcessCPU.User, start))
+	pb.Points = append(pb.Points, addFloat(labels, "runtime.go.cpu.sys", m.ProcessCPU.System, start))
 
-	labels = map[string]string{
-		"name": "mem",
+	labels = []*collectorpb.KeyValue{
+		&collectorpb.KeyValue{
+			Key:   "name",
+			Value: &collectorpb.KeyValue_StringValue{StringValue: "mem"},
+		},
 	}
-	pb.Points = append(pb.Points, addUint(labels, "mem.available", m.Memory.Available))
-	pb.Points = append(pb.Points, addUint(labels, "mem.used", m.Memory.Used))
+	pb.Points = append(pb.Points, addUint(labels, "mem.available", m.Memory.Available, start))
+	pb.Points = append(pb.Points, addUint(labels, "mem.total", m.Memory.Used, start))
 
 	for label, cpu := range m.CPU {
-		labels := map[string]string{
-			"name": label,
+		labels = []*collectorpb.KeyValue{
+			&collectorpb.KeyValue{
+				Key:   "name",
+				Value: &collectorpb.KeyValue_StringValue{StringValue: label},
+			},
 		}
 
-		pb.Points = append(pb.Points, addFloat(labels, "cpu.system", cpu.System))
-		pb.Points = append(pb.Points, addFloat(labels, "cpu.user", cpu.User))
-		pb.Points = append(pb.Points, addFloat(labels, "cpu.idle", cpu.Idle))
-		pb.Points = append(pb.Points, addFloat(labels, "cpu.steal", cpu.Steal))
-		pb.Points = append(pb.Points, addFloat(labels, "cpu.nice", cpu.Nice))
+		pb.Points = append(pb.Points, addFloat(labels, "cpu.sys", cpu.System, start))
+		pb.Points = append(pb.Points, addFloat(labels, "cpu.user", cpu.User, start))
+		pb.Points = append(pb.Points, addFloat(labels, "cpu.idle", cpu.Idle, start))
+		pb.Points = append(pb.Points, addFloat(labels, "cpu.steal", cpu.Steal, start))
+		pb.Points = append(pb.Points, addFloat(labels, "cpu.nice", cpu.Nice, start))
 	}
 	for label, nic := range m.NIC {
-		labels := map[string]string{
-			"name": label,
+		labels = []*collectorpb.KeyValue{
+			&collectorpb.KeyValue{
+				Key:   "name",
+				Value: &collectorpb.KeyValue_StringValue{StringValue: label},
+			},
 		}
-		pb.Points = append(pb.Points, addUint(labels, "net.recv", nic.BytesReceived))
-		pb.Points = append(pb.Points, addUint(labels, "net.sent", nic.BytesSent))
+		pb.Points = append(pb.Points, addUint(labels, "net.bytes_recv", nic.BytesReceived, start))
+		pb.Points = append(pb.Points, addUint(labels, "net.bytes_sent", nic.BytesSent, start))
 	}
 
 	b, err := proto.Marshal(pb)
@@ -171,6 +189,7 @@ func (r *Reporter) Measure(ctx context.Context) error {
 		return err
 	}
 	defer res.Body.Close()
+	r.stored = m
 
 	return nil
 }
